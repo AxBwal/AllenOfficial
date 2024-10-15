37d8e39fe01c81991906932db2145972f75811ff
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-10-15 16:25:05 +0530

    Navbar dropdown added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { PiPhoneCallBold } from "react-icons/pi";
 import logo from "../assets/logo_dark.svg"
 
 function Navbar() {
+  const [dropdown, setDropdown] = useState(null)
+
+  function handleMouseEnter(menu) {
+    setDropdown(menu)
+  }
+
+  function handlemouseleave() {
+    setDropdown(null)
+  }
+
   return (
     <div>
 
@@ -14,11 +24,74 @@ function Navbar() {
         </div>
 
         <div className='flex justify-evenly gap-6 font-semibold'>
-          <div>Exams</div>
-          <div>Programs</div>
-          <div>Scholarships</div>
-          <div>Test Series</div>
-          <div>Study Materials</div>
+
+          <div onMouseEnter={() => handleMouseEnter("Exams")}
+            onMouseLeave={handlemouseleave}
+          >Exams
+            {
+              dropdown === "Exams" && (
+                <div className='absolute mt-2 p-3 bg-[#274278]'>
+                  <div>NEET</div>
+                  <div>JEE</div>
+                  <div>Class 6-10</div>
+                </div>
+              )
+            }
+          </div>
+
+          <div onMouseEnter={() => handleMouseEnter("Programs")}
+            onMouseLeave={handlemouseleave}
+          >Programs
+            {
+              dropdown === "Programs" && (
+                <div className='absolute mt-2 p-3 bg-[#274278]'>
+                  <div>Online Programms</div>
+                  <div>Classroom Programms</div>
+                  <div>Distance Learning</div>
+                </div>
+              )
+            }
+          </div>
+          <div onMouseEnter={() => handleMouseEnter("Scholarships")}
+            onMouseLeave={handlemouseleave}>Scholarships
+
+            {
+              dropdown === "Scholarships" && (
+                <div className='absolute mt-2 p-3 bg-[#274278]'>
+                  <div>ADSAT</div>
+                  <div>TALLENTEX</div>
+                </div>
+              )
+            }
+          </div>
+          <div onMouseEnter={() => handleMouseEnter("TestSeries")}
+            onMouseLeave={handlemouseleave}>Test Series
+            {
+              dropdown === "TestSeries" && (
+                <div className='absolute mt-2 p-3 bg-[#274278]'>
+                  <div>NEET</div>
+                  <div>JEE(MAIN + ADVANCE)</div>
+                  <div>JEE main</div>
+                </div>
+              )
+            }
+          </div>
+          <div onMouseEnter={() => handleMouseEnter("StudyMaterials")}
+            onMouseLeave={handlemouseleave}>Study Materials
+            {
+              dropdown === "StudyMaterials" && (
+                <div className='absolute mt-2 p-3 bg-[#274278]'>
+                  <div>JEE MAIN</div>
+                  <div>JEE Advanced</div>
+                  <div>NEET</div>
+                  <div>NCERT Solutions</div>
+                  <div>CBSE</div>
+                  <div>Olympiad</div>
+
+                </div>
+              )
+            }
+          </div>
         </div>
 
         <div className='flex justify-evenly gap-6'>
