516e25d36ea9a5ed2bb9e18b19169fcc98b75276
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-10-15 20:28:23 +0530

    updated Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { PiPhoneCallBold } from "react-icons/pi";
 import logo from "../assets/logo_dark.svg"
+import { MdKeyboardArrowRight } from "react-icons/md";
 
 function Navbar() {
   const [dropdown, setDropdown] = useState(null)
@@ -14,7 +15,7 @@ function Navbar() {
   }
 
   return (
-    <div>
+    <div className='cursor-pointer'>
 
       <div className='flex justify-around px-6 py-7 mt-2 items-center'>
         <div>
@@ -30,13 +31,23 @@ function Navbar() {
           >Exams
             {
               dropdown === "Exams" && (
-                <div className='absolute mt-2 p-3 bg-[#274278]'>
-                  <div>NEET</div>
-                  <div>JEE</div>
-                  <div>Class 6-10</div>
+                <div className='absolute mt-2 p-3 bg-[#274278] space-y-2 rounded-lg'>
+                  <div className='flex justify-between items-center p-2 hover:bg-[#1e355f] rounded-md'>
+                    <div>NEET</div>
+                    <MdKeyboardArrowRight />
+                  </div>
+                  <div className='flex justify-between items-center p-2 hover:bg-[#1e355f] rounded-md'>
+                    <div>JEE</div>
+                    <MdKeyboardArrowRight />
+                  </div>
+                  <div className='flex justify-between items-center p-2 hover:bg-[#1e355f] rounded-md'>
+                    <div>Class 6-10</div>
+                    <MdKeyboardArrowRight />
+                  </div>
                 </div>
               )
             }
+
           </div>
 
           <div onMouseEnter={() => handleMouseEnter("Programs")}
@@ -44,10 +55,10 @@ function Navbar() {
           >Programs
             {
               dropdown === "Programs" && (
-                <div className='absolute mt-2 p-3 bg-[#274278]'>
-                  <div>Online Programms</div>
-                  <div>Classroom Programms</div>
-                  <div>Distance Learning</div>
+                <div className='absolute mt-2 p-3 bg-[#274278] rounded-lg space-y-2'>
+                  <div className='p-2'>Online Programms</div>
+                  <div className='p-2'>Classroom Programms</div>
+                  <div className='p-2'>Distance Learning</div>
                 </div>
               )
             }
@@ -57,9 +68,9 @@ function Navbar() {
 
             {
               dropdown === "Scholarships" && (
-                <div className='absolute mt-2 p-3 bg-[#274278]'>
-                  <div>ADSAT</div>
-                  <div>TALLENTEX</div>
+                <div className='absolute mt-2 p-3 bg-[#274278] rounded-lg space-y-2'>
+                  <div className='p-1'>ADSAT</div>
+                  <div className='p-1'>TALLENTEX</div>
                 </div>
               )
             }
@@ -68,10 +79,10 @@ function Navbar() {
             onMouseLeave={handlemouseleave}>Test Series
             {
               dropdown === "TestSeries" && (
-                <div className='absolute mt-2 p-3 bg-[#274278]'>
-                  <div>NEET</div>
-                  <div>JEE(MAIN + ADVANCE)</div>
-                  <div>JEE main</div>
+                <div className='absolute mt-2 p-3 bg-[#274278] space-y-2 rounded-lg'>
+                  <div className='flex justify-between items-center p-2 '><div>NEET</div> <div><MdKeyboardArrowRight /></div></div>
+                  <div className='flex justify-between items-center p-2 '><div>JEE(MAIN + ADVANCE)</div> <span><MdKeyboardArrowRight /></span></div>
+                  <div className='flex justify-between items-center p-2 '><div>JEE main</div> <span><MdKeyboardArrowRight /></span></div>
                 </div>
               )
             }
@@ -80,13 +91,13 @@ function Navbar() {
             onMouseLeave={handlemouseleave}>Study Materials
             {
               dropdown === "StudyMaterials" && (
-                <div className='absolute mt-2 p-3 bg-[#274278]'>
-                  <div>JEE MAIN</div>
-                  <div>JEE Advanced</div>
-                  <div>NEET</div>
-                  <div>NCERT Solutions</div>
-                  <div>CBSE</div>
-                  <div>Olympiad</div>
+                <div className='absolute mt-2 p-3 bg-[#274278] space-y-2 rounded-lg'>
+                  <div className='flex justify-between items-center p-2'><div>JEE MAIN</div> <MdKeyboardArrowRight /></div>
+                  <div className='flex justify-between items-center p-2'><div>JEE Advanced</div> <MdKeyboardArrowRight /></div>
+                  <div className='flex justify-between items-center p-2'><div>NEET</div> <MdKeyboardArrowRight /></div>
+                  <div className='flex justify-between items-center p-2'><div>NCERT Solutions</div> <MdKeyboardArrowRight /></div>
+                  <div className='flex justify-between items-center p-2'><div>CBSE</div> <MdKeyboardArrowRight /></div>
+                  <div className='flex justify-between items-center p-2'><div>Olympiad</div> <MdKeyboardArrowRight /></div>
 
                 </div>
               )
